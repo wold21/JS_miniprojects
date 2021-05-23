068bc66f4d344dcc532771b2ad9c08f87bc01392
wold21 <wold2180@gmail.com>
2021-05-23 21:59:17 +0900

    변환된 css

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -1,8 +1,10 @@
 window.onload = function () {
+  var mins = 00;
   var seconds = 00;
   var tens = 00;
   var appendTens = document.getElementById("tens");
   var appendSeconds = document.getElementById("seconds");
+  var appendMins = document.getElementById("min");
   var btnStart = document.getElementById("btn-start");
   var btnStop = document.getElementById("btn-stop");
   var btnReset = document.getElementById("btn-reset");
@@ -21,8 +23,10 @@ window.onload = function () {
     clearInterval(interval);
     tens = "00";
     seconds = "00";
+    mins = "00";
     appendTens.innerHTML = tens;
     appendSeconds.innerHTML = seconds;
+    appendMins.innerHTML = mins;
   };
 
   function startTimer() {
@@ -46,6 +50,17 @@ window.onload = function () {
 
     if (seconds > 9) {
       appendSeconds.innerHTML = seconds;
+    }
+
+    if (seconds > 60) {
+      console.log(mins);
+      mins++;
+      appendMins.innerHTML = "0" + mins;
+      seconds = 0;
+      appendSeconds.innerHTML = "0" + 0;
+    }
+    if (mins > 60) {
+      appendMins.innerHTML = mins;
     }
   }
 };
